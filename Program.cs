557d81443ab60311d6c391f4327f8974ead31418
vanely <vanelys94@gmail.com>
2020-02-08 00:49:42 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Learning
+{
+    class Program
+    {
+        public string text;
+        public string myName = "Vanely";
+        public int age = 26;
+        public static void Main(string[] args)
+        {
+            Program context = new Program();
+
+            Console.WriteLine("Hello, my name is "+context.myName+"\nI am "+context.age+" years old!");
+            Console.ReadKey();
+            Console.WriteLine("Still here yall!");
+            context.text = Console.ReadLine();
+
+            if (context.text == "lalala") {
+                Console.WriteLine("You wrote: "+context.text);
+            } else {
+                Console.WriteLine("Nah, you wrote some other shit!");
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
